7cc440e66ca4f9f395b0b1c8c8549018c2bab870
ZHE YANG <zheyang@ZHEdeMacBook-Pro.local>
2019-04-02 11:14:04 +0800

    ejs

---
+++ day2/11.js
@@ -0,0 +1,25 @@
+var ejs = require("ejs");
+var fs = require("fs");
+
+fs.readFile("./views/index.ejs", function(err, data){
+    var template = data.toString();
+    var dic = {a: 6};
+
+    var html = ejs.render(template, dic);
+
+    console.log(html);
+});
+
+/*
+var ejs = require("ejs");
+
+var string = "<%= a %>";
+
+var data = {
+    a: 6,
+};
+
+var html = ejs.render(string, data);
+
+console.log(html);
+*/
