c8371fdf582a8c41394de3854ecbf6f914f1c08a
ZHE YANG <zheyang@ZHEdeMacBook-Pro.local>
2019-04-02 10:31:09 +0800

    day4

---
+++ day4/news/app.js
@@ -0,0 +1,50 @@
+var express = require("express");
+
+var app = express();
+
+var db = [
+    {
+        "title": "woshi 1 hao xinwen",
+        "time": "2019874464633",
+        "neirong": "<p>ldsfjdslkfjlsjflksdjfdjfdsljflksjflkdsjflk</p>"
+    },
+
+    {
+        "title": "woshi 2 hao xinwen",
+        "time": "2019874464633",
+        "neirong": "<p>ldsfjdslkfjlsjflksdjfdjfdsljflksjflkdsjflk</p>"
+    },
+
+    {
+        "title": "woshi 3 hao xinwen",
+        "time": "2019874464633",
+        "neirong": "<p>ldsfjdslkfjlsjflksdjfdjfdsljflksjflkdsjflk</p>"
+    },
+];
+
+app.set("view engine", "ejs");
+
+/*
+app.get("/news/:id", function (req,res) {
+    //新闻id
+    var id = parseInt(req.params.id);
+
+    res.render("haha", db[id]);
+})；
+*/
+
+app.get("/news/:id", function (req,res) {
+    var id = parseInt(req.params.id)-1;
+
+    console.log(db[id]);
+    res.render("haha");
+});
+
+app.get("/test/:id", function (req,res) {
+    var id = parseInt(req.params.id)-1;
+
+    console.log(db[id]);
+    res.render("test");
+});
+
+app.listen(3000);
